58fd8683bb6547577384517e6dce48a5d8f64a8c
FerencziTamas <56951512+FerencziTamas@users.noreply.github.com>
2022-03-20 18:48:33 +0100

    php folder

---
+++ php/backend_template.php
@@ -0,0 +1,22 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_REQUEST['username'];
+    if (empty($name)) {
+      echo "Name is empty";
+    } else {
+      echo $name;
+    }
+  }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $passwd = $_REQUEST['password'];
+    if (empty($passwd)) {
+      echo "Passwd is empty";
+    } else {
+      echo $passwd;
+    }
+}
+  
+
+?>
