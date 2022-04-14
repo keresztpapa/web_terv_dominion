f73f35731c1fa6fde9ed5610f8671689dbdcd803
Ferenc Solyom <ifj.solyom.feco15@gmail.com>
2022-04-14 09:19:31 +0200

    php indeed

---
+++ php/template/footer.php
@@ -0,0 +1,5 @@
+    <!--Footer-->
+    <footer>
+        <p>@Sólyom and Ferenczi Webpage Creators</p>
+        <p><em><strong>Készült a Nelson and Murdock ügyvédi iroda megbízásából.</strong></em></p>
+    </footer>
