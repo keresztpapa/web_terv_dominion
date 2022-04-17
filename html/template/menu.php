fbf77ab24ff6ce6aebb5dce735c57d374861d96d
Ferenc Solyom <ifj.solyom.feco15@gmail.com>
2022-04-17 09:37:51 +0200

    init

--- html/template/menu.php
+++ html/template/menu.php
@@ -1,11 +1,11 @@
  <!--Menüsor-->
  <ul>
-        <li><a href="index.html">Főoldal</a></li>
-        <li><a href="daredevil.html" id="menupont">Daredevil</a></li>
-        <li><a href="jessicaJones.html">Jessica Jones</a></li>
-        <li><a href="lukeCage.html">Luke Cage</a></li>
-        <li><a href="ironFist.html">Iron Fist</a></li>
-        <li><a href="defenders.html">The Defenders</a></li>
-        <li><a href="punisher.html">The Punisher</a></li>
-        <li><a href="bejelentkezes.html">Bejelentkezés</a></li>
+        <li><a href="index.php">Főoldal</a></li>
+        <li><a href="daredevil.php" id="menupont">Daredevil</a></li>
+        <li><a href="jessicaJones.php">Jessica Jones</a></li>
+        <li><a href="lukeCage.php">Luke Cage</a></li>
+        <li><a href="ironFist.php">Iron Fist</a></li>
+        <li><a href="defenders.php">The Defenders</a></li>
+        <li><a href="punisher.php">The Punisher</a></li>
+        <li><a href="bejelentkezes.php">Bejelentkezés</a></li>
     </ul>
