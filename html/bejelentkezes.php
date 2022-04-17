af39f0cad733a8cfe22d3b778710e5b87f7dc93d
Ferenc Solyom <ifj.solyom.feco15@gmail.com>
2022-04-17 10:05:13 +0200

    check for login

--- html/bejelentkezes.php
+++ html/bejelentkezes.php
@@ -2,7 +2,7 @@
     include_once "UserClass.php";
     session_start();
 
-    $file = fopen("../data/hipertitkosformatum.txt", "rw");
+    $file = fopen("data/hipertitkosformatum.txt", "rw");
     $adatok = [];
 
     if (!$file) {
