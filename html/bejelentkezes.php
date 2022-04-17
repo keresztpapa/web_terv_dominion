28af119f0c4a4b602636888d2ff09b71d0211c1e
Ferenc Solyom <ifj.solyom.feco15@gmail.com>
2022-04-17 10:21:58 +0200

    init

--- html/bejelentkezes.php
+++ html/bejelentkezes.php
@@ -70,6 +70,63 @@
             $siker = FALSE;
         }
     }
+
+
+    class User{
+        public string $username;
+        public string $passwd;  
+      
+        public function __construct(string $usrnm, string $pass){
+            $this->username = $usrnm;
+            $this->passwd = $pass;
+        }
+      
+        public function getUsername():string {
+            return $this->username;
+        }
+      
+        public function getPassword():string {
+            return $this->passwd;
+        }
+      
+        public function setUsername(string $asd): void {
+          $this->username = $asd;
+        }
+      
+        public function setPasswd(string $asd): void{
+            $this->passwd = $asd; 
+        }
+      
+        public function loadUsers($path) {
+          $users = [];
+      
+          $file = fopen($path, "r");
+          if ($file === FALSE)
+            die("Nem sikerült megnyitni a fajlt!");
+      
+          while (($line = fgets($file)) !== FALSE) {
+            $user = unserialize($line);
+            $users[] = $user;
+          }
+      
+          fclose($file);
+          return $users;
+        }
+      
+        public function saveUsers($path, $users) {
+          $file = fopen($path, "a");
+          if ($file === FALSE)
+            die("Nem sikerült megnyitni a fajlt!");
+      
+          foreach($users as $user) {
+            $serialized_user = serialize($user);
+            fwrite($file, $serialized_user . "\n");
+          }
+      
+          fclose($file);
+        }
+      }
+
 ?>
 
 <!DOCTYPE html>
