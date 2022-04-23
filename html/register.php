1e641d0f387bac69175031cf65c2329568a8dd8d
FerencziTamas <56951512+FerencziTamas@users.noreply.github.com>
2022-04-23 17:56:19 +0200

    Regisztracio dupla jelszoval

--- html/register.php
+++ html/register.php
@@ -1,85 +1,48 @@
 <?php
+    include "template/fajlBeKi.php";
+    $fiokok=loadUsers();
 
- 
-$fiokok = [
-    ["usernameReg" => "admin", "jepasswordReglszo" => "admin"],
-    ["usernameReg" => "guest", "passwordReg" => "guest"],
-    ["usernameReg" => "TestUser", "passwordReg" => "test"]
-  ];
+    $hibak = [];
   
-  
-  function loadUsers($path) {
-    $users = [];
-  
-    $file = fopen($path, "r");
-    if ($file === FALSE)
-        die("Nem sikerült megnyitni a fajlt!");
-  
-    while (($line = fgets($file)) !== FALSE) {
-        $user = unserialize($line);
-        $users[] = $user;
+    if (isset($_POST["register"])) {
+        if (!isset($_POST["usernameReg"]) || trim($_POST["usernameReg"]) === ""){
+            $hibak[] = "A felhasználónév megadása kötelező!";
+        }
+    
+        if (!isset($_POST["passwordReg"]) || trim($_POST["passwordReg"]) === "" || !isset($_POST["passwordAgain"]) || trim($_POST["passwordAgain"]) === ""){
+            $hibak[] = "A jelszó és az ellenőrző jelszó megadása kötelező!";
+        }
+    
+        $felhasznalonev = $_POST["usernameReg"];
+        $jelszo = $_POST["passwordReg"];
+        $jelszo2 = $_POST["passwordAgain"];
+    
+    
+        foreach ($fiokok as $fiok) {
+            if ($fiok["usernameReg"] === $felhasznalonev){
+                $hibak[] = "A felhasználónév már foglalt!";
+            } 
+        }
+    
+        if (strlen($jelszo) < 8) {
+            $hibak[] = "A jelszónak legalább 8 karakter hosszúnak kell lennie!";
+        } 
+    
+        if ($jelszo !== $jelszo2) {
+            $hibak[] = "A jelszavak nem egyeznek!";
+        } 
+    
+        if (count($hibak) === 0) {
+            $jelszo = password_hash($jelszo, PASSWORD_DEFAULT);
+            $fiokok[] = ["usernameReg" => $felhasznalonev, "passwordReg" => $jelszo];
+            saveUsers($fiokok);
+            $siker = TRUE;
+        } else {
+            $siker = FALSE;
+            print_r($hibak);
+        }
     }
-  
-    fclose($file);
-    return $users;
-  }
-  
-  function saveUser($path, $felhasznalonev, $jelszo) {
-    $file = fopen($path, "a");
-    if ($file === FALSE) die("Nem sikerült megnyitni a fajlt!");
-  
-    fwrite($file, $felhasznalonev . "\n");
-    fwrite($file, $jelszo . "\n");
-  
-    fclose($file);
-  }
-  
-  session_start();
-  $hibak = [];
-  
-  $text = fopen("data/hipertitkosformatum.txt", "r") or die("Unable to open file!");
-  
-  //$fiokok = fread($text,filesize("data/hipertitkosformatum.txt"));
-  
-  
-  if (isset($_POST["SubmitReg"])) {
-    if (!isset($_POST["usernameReg"]) || trim($_POST["usernameReg"]) === ""){
-        $hibak[] = "A felhasználónév megadása kötelező!";
-    }
-  
-    if (!isset($_POST["passwordReg"]) || trim($_POST["passwordReg"]) === "" || !isset($_POST["passwordAgain"]) || trim($_POST["passwordAgain"]) === ""){
-        $hibak[] = "A jelszó és az ellenőrző jelszó megadása kötelező!";
-    }
-  
-    $felhasznalonev = $_POST["usernameReg"];
-    $jelszo = $_POST["passwordReg"];
-    $jelszo2 = $_POST["passwordAgain"];
-  
-  
-    foreach ($fiokok as $fiok) {
-      if ($fiok["usernameReg"] === $felhasznalonev) $hibak[] = "A felhasználónév már foglalt!";
-    }
-  
-    if (strlen($jelszo) < 8) $hibak[] = "A jelszónak legalább 8 karakter hosszúnak kell lennie!";
-  
-    if ($jelszo !== $jelszo2) $hibak[] = "A jelszó és az ellenőrző jelszó nem egyezik!";
-  
-    if (count($hibak) === 0) {
-        $jelszo = password_hash($jelszo, PASSWORD_DEFAULT);
-        $fiokok[] = ["usernameReg" => $felhasznalonev, "passwordReg" => $jelszo];
-  
-        saveUser("data/hipertitkosformatum.txt", $felhasznalonev, $jelszo);
-  
-        $siker = TRUE;
-    } else {
-        $siker = FALSE;
-        print_r($hibak);
-    }
-  }
-  
-  
-  fclose($text);
-  ?>
+?>
 
 <!DOCTYPE html>
 <html lang="hu">
@@ -175,7 +138,7 @@ $fiokok = [
 
     <?php
         if (isset($siker) && $siker === TRUE) {
-            alert("Sikeres regisztráció!");
+            echo '<script>alert("Sikeres Regisztráció")</script>';
         }
     ?>
 
