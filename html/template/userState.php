7369d7b74746663fc693f83ca2b8ef654127adfe
ifj.solyom.feco15@gmail.com <ifj.solyom.feco15@gmail.com>
2022-04-26 20:57:41 +0200

    minor progress

--- html/template/userState.php
+++ html/template/userState.php
@@ -1,9 +1,13 @@
 <div id="user_sync">
     <?php 
-        if($_SESSION["userName"] != ""){
-            echo $_SESSION["userName"];
-        } else {
-            echo "Anonymus";
-        }
+
+         if (session_status() == PHP_SESSION_NONE) {session_start();}        
+         try{   
+            if(array_key_exists("userName", $_SESSION)){
+                echo $_SESSION["userName"];
+            }
+         }catch (Exception $e){
+            echo "rips";
+         }
     ?>
 </div>
