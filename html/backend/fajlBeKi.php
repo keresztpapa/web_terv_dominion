373fcf7d0997d92c42f5a179bfe1a7273e3b8930
ifj.solyom.feco15@gmail.com <ifj.solyom.feco15@gmail.com>
2022-04-27 21:42:30 +0200

    minor progress

--- html/backend/fajlBeKi.php
+++ html/backend/fajlBeKi.php
@@ -1,6 +1,32 @@
 <?php 
   include "backend/UserClass.php";
 
+  function loadUsers(string $filename): User{
+    $file = fopen($filename, "r");
+    if ($file === FALSE)
+      die("Nem sikerült megnyitni a fajlt!");
+    while (($line = fgets($file)) !== FALSE) {
+      $user = unserialize($line);
+    }
+
+    fclose($file);
+    return $user;
+  }
+
+
+  function saveUsers(string $filename, User $user){
+    $file = fopen($filename, "w");
+    if($file === FALSE){
+      die("rip");
+    }else{
+      $serialized_user = serialize($user);
+      fwrite($file, $serialized_user."\n");
+    }
+    fclose($file);
+  }
+
+  /*
+
   function loadUsers(string $filename): array{
     $users = [];
 
@@ -17,18 +43,6 @@
     return $users;
   }
 
-  function saveUsers(string $filename, User $user){
-    $file = fopen($filename, "w");
-    if($file === FALSE){
-      die("rip");
-    }else{
-      $serialized_user = serialize($user);
-      fwrite($file, $serialized_user."\n");
-    }
-    fclose($file);
-  }
-
-  /*
   function saveUsers(string $filename, array $users){
     $file = fopen($filename, "w");
     if ($file === FALSE){
